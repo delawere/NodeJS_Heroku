d579882e4afe233a0cbdea23cc35f7567b18b9c0
Dmitriy Zhiganov <delawere2009@yandex.ru>
2020-06-22 21:53:06 +0300

    add port

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../public/index.html`));
 });
 
-app.listen(3000, () =>  {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 5000, () =>  {
+  console.log('Starting!');
 });
