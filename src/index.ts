3e6b7227ae7b41c8d951e6ad0b4672386ed0b141
Dmitriy Zhiganov <delawere2009@yandex.ru>
2020-06-22 21:49:22 +0300

    feat

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import path from 'path';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/public/index.html`));
+  res.sendFile(path.join(`${__dirname}/../public/index.html`));
 });
 
 app.listen(3000, () =>  {
